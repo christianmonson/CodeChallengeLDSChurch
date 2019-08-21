c54fd0ee3f874484d7a7202aebdca51d363f346b
christianrmonson <christianrmonson@gmail.com>
2019-08-21 17:28:42 -0600

    Initial Commit

---
+++ Code Challenge-LDS Church/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Code Challenge-LDS Church
+//
+//  Created by Christian R Monson on 8/21/19.
+//  Copyright © 2019 christianrmonson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
